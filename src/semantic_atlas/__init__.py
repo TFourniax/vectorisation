02675662700b419073ee8c465c3aabd8cc699c77
TFourniax <129301290+TFourniax@users.noreply.github.com>
2026-08-17 01:19:07 +0200

    export semantic fabric API

--- src/semantic_atlas/__init__.py
+++ src/semantic_atlas/__init__.py
@@ -1,7 +1,11 @@
 """Semantic Manifold Atlas research prototype."""
 
+from .alignment import TransitionAtlas, TransitionDiagnostics, TransitionGraph
 from .core import AtlasIndex, AtlasRecord, SearchHit, SearchPolicy
+from .drift import DriftPoint, DriftReport, compare_indexes
 from .embed import FeatureHashEmbedder
+from .fabric import FabricRecord, SemanticCell, SemanticFabric, SpaceSpec
+from .transition_io import load_transition, save_transition
 
 __all__ = [
     "AtlasIndex",
@@ -9,4 +13,16 @@ __all__ = [
     "SearchHit",
     "SearchPolicy",
     "FeatureHashEmbedder",
+    "TransitionAtlas",
+    "TransitionDiagnostics",
+    "TransitionGraph",
+    "FabricRecord",
+    "SemanticCell",
+    "SemanticFabric",
+    "SpaceSpec",
+    "DriftPoint",
+    "DriftReport",
+    "compare_indexes",
+    "save_transition",
+    "load_transition",
 ]
