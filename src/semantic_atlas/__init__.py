8193766af6c6adf039dc841aa043e510e6ac80d9
TFourniax <129301290+TFourniax@users.noreply.github.com>
2026-08-17 01:34:59 +0200

    export Semantic ABI public API

--- src/semantic_atlas/__init__.py
+++ src/semantic_atlas/__init__.py
@@ -1,6 +1,18 @@
 """Semantic Manifold Atlas research prototype."""
 
 from .alignment import RoutedTransport, TransitionAtlas, TransitionDiagnostics, TransitionGraph
+from .contracts import (
+    ContractLedger,
+    ContractReport,
+    GateDecision,
+    MutualNeighborClause,
+    NeighborClause,
+    SemanticABIGate,
+    SemanticContract,
+    TripletClause,
+    capture_behavior_contract,
+    contract_from_labels,
+)
 from .core import AtlasIndex, AtlasRecord, SearchHit, SearchPolicy
 from .drift import DriftPoint, DriftReport, compare_indexes
 from .embed import FeatureHashEmbedder
@@ -26,4 +38,14 @@ __all__ = [
     "compare_indexes",
     "save_transition",
     "load_transition",
+    "SemanticContract",
+    "TripletClause",
+    "NeighborClause",
+    "MutualNeighborClause",
+    "ContractReport",
+    "ContractLedger",
+    "SemanticABIGate",
+    "GateDecision",
+    "capture_behavior_contract",
+    "contract_from_labels",
 ]
